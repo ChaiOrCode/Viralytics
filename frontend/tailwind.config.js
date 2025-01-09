7e7d85d1139104013522cfb0c244d642f05213df
Sans11Pentium <sanskriti.ug22@nsut.ac.in>
2025-01-09 19:42:22 +0530

    error resolved

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,36 +7,35 @@ export default {
     extend: {
       colors: {
         light: {
-          primary: "#31b4f2",
-          secondary: "#0000ff",
-          tertiary: "#025492",
-          background: "#ffffff",
+          primary: '#31b4f2',
+          secondary: '#0000ff',
+          tertiary: '#025492',
+          background: '#ffffff'
         },
         dark: {
-          primary: "#31b4f2",
-          secondary: "#0000ff",
-          tertiary: "#025492",
-          background: "#1a1a1a",
+          primary: '#31b4f2',
+          secondary: '#0000ff',
+          tertiary: '#025492',
+          background: '#1a1a1a'
         },
         text: {
-          dark: "#31b4f2",
-          light: "#0000ff",
-          dim: "#025492",
-          fade: "#f8cf40",
+          dark: '#31b4f2',
+          light: '#0000ff',
+          dim: '#025492',
+          fade: '#f8cf40'
         },
         nav: {
-          dark: "#1a1a1a",
-          light: "#ffffff",
-        },
+          dark: '#1a1a1a',
+          light: '#ffffff'
+        }
       },
       backgroundImage: {
-        noise:
-          'url("data:image/svg+xml,%3Csvg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg"%3E%3Cfilter id="noise"%3E%3CfeTurbulence type="fractalNoise" baseFrequency="0.65" numOctaves="3" stitchTiles="stitch"/%3E%3C/filter%3E%3Crect width="100%" height="100%" filter="url(%23noise)" opacity="0.075"/%3E%3C/svg%3E")',
+        'noise': 'url("data:image/svg+xml,%3Csvg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg"%3E%3Cfilter id="noise"%3E%3CfeTurbulence type="fractalNoise" baseFrequency="0.65" numOctaves="3" stitchTiles="stitch"/%3E%3C/filter%3E%3Crect width="100%" height="100%" filter="url(%23noise)" opacity="0.075"/%3E%3C/svg%3E")',
       },
       opacity: {
-        85: "0.85",
-      },
+        '85': '0.85',
+      }
     },
   },
   plugins: [],
-};
+}
